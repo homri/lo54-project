6681883fc43fd2a71081c93ed24c32303a2a0ae2
Iki <felix.lahemade@gmail.com>
2016-06-09 09:13:17 +0200

    All types of sorting are working.

--- src/main/java/fr/utbm/servlet/FormationsServlet.java
+++ src/main/java/fr/utbm/servlet/FormationsServlet.java
@@ -31,7 +31,7 @@ public class FormationsServlet extends HttpServlet {
         response.setContentType( "text/html" );
         PrintWriter out = response.getWriter();
 
-        SimpleDateFormat formater = new SimpleDateFormat("dd MMMM yyyy");
+        SimpleDateFormat formater = new SimpleDateFormat("MM/dd/yyyy");
 
         String type=request.getParameter("type");
         List<Course> courses=null;
@@ -45,13 +45,14 @@ public class FormationsServlet extends HttpServlet {
             }
             courses = courseController.getAllCoursesAtDateController(date);
         }else if(type.equals("place-sorting")) {
-            int id = Integer.getInteger(request.getParameter("place"));
+            String res= request.getParameter("place");
+            int id = Integer.parseInt(res);
             Location location = locationController.getLocationById(id);
-            //courses = courseController.getAllCoursesAtLocationController (location);
+            courses = courseController.getAllCoursesAtLocationController (location);
         }else if(type.equals("filter-sorting")) {
             courses = courseController.getAllCoursesPerKeyWordController(request.getParameter("keyword"));
         }else{
-                out.println("Lolilol : erreur; ");
+                out.println("On n'applique pas bien le filtre.");
         }
 
         List<Location> locations = LocationController.getAllLocations();
@@ -73,13 +74,7 @@ public class FormationsServlet extends HttpServlet {
         out.println( "</HEAD>" );
         out.println( "<BODY>" );
         out.println( "<H1>Liste des formations disponibles : </H1>" );
-        out.println("<div class=\"sort\"><span>Trier par :</span>" +
-                "<ul class=\"sort-list\">" +
-                "<li><a id=\"sort-by-date\">Date<img src=\"styles/svg/note.svg\"></a></li>" +
-                "<li><a id=\"sort-by-place\">Lieu<img src=\"styles/svg/pin.svg\"></a></li>" +
-                "<li><a id=\"sort-by-search\">Mot-Clé<img src=\"styles/svg/browser-visualization.svg\"></a></li>" +
-                "</ul>" +
-                "</div>");
+
         for (Course course : courses) {
             out.println("<b>" + course.getCourse_code() + " - " + course.getTitle() + "</b>");
             out.println("<br>");
@@ -93,34 +88,7 @@ public class FormationsServlet extends HttpServlet {
             }
             out.println("<br>");
         }
-        out.println("<div class=\"date-sorting modale\">" +
-                "<form action=\"./formations\" method=\"post\" role=\"form\">" +
-                "<input type=\"hidden\" name=\"type\" value=\"date-sorting\">" +
-                "Choisir une date : <input type=\"text\"  id=\"datepicker\">" +
-                "<button type=\"submit\" class=\"btn btn-primary\">Valider</button>" +
-                "<img src=\"styles/svg/close.svg\" class=\"close-modale\" />" +
-                "</form>" +
-                "</div>");
-        out.println("<div class=\"place-sorting modale\">" +
-                "<form action=\"./formations\" method=\"post\" role=\"form\">" +
-                "<input type=\"hidden\" name=\"type\" value=\"place-sorting\">" +
-                "<select name=\"place\" class=\"form-control\">\n");
-        for (Location location : locations) {
-            out.println("<option>" + location.getId_location() + " - " + location.getCity() + "</option>");
-        }
-        out.println("</select>" +
-                "<button type=\"submit\" class=\"btn btn-primary\">Valider</button>" +
-                "</form>" +
-                "<img src=\"styles/svg/close.svg\" class=\"close-modale\"/>" +
-                "</div>");
-        out.println("<div class=\"filter-sorting modale\">" +
-                "<form action=\"./formations\" method=\"post\" role=\"form\">" +
-                "<input type=\"hidden\" name=\"type\" value=\"filter-sorting\">" +
-                "Mot-clé : <input type=\"text\"  id=\"keyword\">" +
-                "<button type=\"submit\" class=\"btn btn-primary\">Valider</button>" +
-                "<img src=\"styles/svg/close.svg\" class=\"close-modale\"/>" +
-                "</form>" +
-                "</div>");
+        out.println("<a href=\"./formations\">Retour à la liste complète</a>");
         out.println("<script src=\"../js/script.js\"></script>");
         out.println( "</BODY>" );
         out.println( "</HTML>" );
